7bbcd53a61e2cbb48222972da0973334caecad18
Shiya Kohn <shiyaliebling@gmail.com>
2023-04-23 19:08:27 -0400

    Add login and stuff, veoid veoid

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,42 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using System.Security.Claims;
+using System.Text;
+using Api.Models;
 using Api.Services;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
-builder.Services.AddSingleton<WeatherApiClient>(container => 
+builder.Services
+    .AddAuthentication(configureOptions: options =>
+    {
+        options.DefaultAuthenticateScheme = "Local";
+    })
+    .AddJwtBearer("Local", configuration =>
+    {
+        configuration.TokenValidationParameters = new TokenValidationParameters
+        {
+
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true,
+
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Authentication:Secret"]))
+        };
+    });
+
+builder.Services.AddAuthorization(configurator => configurator
+    .AddPolicy("staff_policy", policy => policy.RequireRole("staff"))
+);
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddSingleton<WeatherApiClient>(container =>
 {
     var key = container.GetRequiredService<IConfiguration>().GetValue<string>("WeatherApi:Token");
 
@@ -15,15 +47,56 @@ builder.Services.AddSingleton<WeatherApiClient>(container =>
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapPost("/login", (LoginRequest login, IConfiguration configuration) =>
+{
+    var secret = configuration["Authentication:Secret"];
+
+    ArgumentNullException.ThrowIfNull(secret, "Authentication:Secret is required");
+
+    var token = GenrateToken(secret, login.Role);
+
+    return Results.Ok(new { token });
+});
+
 app.MapGet("/", () =>
 {
-    return "Please enter a zip code, e.g. /12345";
+    var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+    return Results.File(path + ".\\frontend\\index.html", "text/html");
 });
 
-
-app.MapGet("/{zipcode}", (string zipcode, WeatherApiClient apiClient) =>
+app.MapGet("/{zipcode:long}", async (string zipcode, WeatherApiClient apiClient) =>
 {
-    return apiClient.GetWeatherAsync(zipcode);
-});
+    return await apiClient.GetWeatherAsync(zipcode);
+}).RequireAuthorization("staff_policy");
+
+app.MapGet("/{anythingelse}", (string anythingelse) =>
+{
+    return Results.NotFound($"{anythingelse} is not a valid zip code. Please try again.");
+}).RequireAuthorization();
 
 app.Run();
+
+string GenrateToken(string secret, string role)
+{
+    var tokenHandler = new JwtSecurityTokenHandler();
+
+    var key = Encoding.ASCII.GetBytes(secret);
+
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Subject = new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.Role, role)
+        }),
+        Expires = DateTime.UtcNow.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+    };
+
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+    return tokenHandler.WriteToken(token);
+}
--- Api/Services/WeatherApiClient.cs
+++ Api/Services/WeatherApiClient.cs
@@ -1,5 +1,7 @@
 namespace Api.Services;
 
+using System.Text.Json.Serialization;
+
 public class WeatherApiClient
 {
     private const string BaseUrl = "https://api.weatherapi.com/v1/current.json";
@@ -13,14 +15,44 @@ public class WeatherApiClient
         this.client = client;
     }
 
-    public Task<Weather?> GetWeatherAsync(string zipcode)
+    public async Task<Weather> GetWeatherAsync(string zipcode)
     {
         var url = $"{BaseUrl}?q={zipcode}&key={apiKey}&aqi=no";
 
-        return client.GetFromJsonAsync<Weather>(url);
+        var weather = await client.GetFromJsonAsync<WeatherResponse>(url)
+            ?? throw new Exception("Weather API returned null");
+
+        return new Weather
+        {
+            Location = new Weather.LocationModel
+            {
+                City = weather.Location.Name,
+                State = weather.Location.Region,
+            }
+        };
     }
 
     public class Weather
-    {        
+    {
+        public LocationModel Location { get; set; }
+
+        public class LocationModel
+        {
+            public string City { get; set; }
+            public string State { get; set; }
+
+        }
+    }
+
+    public class WeatherResponse
+    {
+        public LocationModel Location { get; set; }
+
+
+        public class LocationModel
+        {
+            public string Name { get; set; }
+            public string Region { get; set; }
+        }
     }
 }
